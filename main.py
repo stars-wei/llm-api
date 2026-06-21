ef52568b98895ce6ba987921d01565a379529b44
Developer <developer@example.com>
2026-06-21 16:55:28 +0800

    feat: save chat history to local file

--- main.py
+++ main.py
@@ -11,4 +11,9 @@ while True:
     answer = ask_deepseek(question)
     print(answer)
 
+    with open("chat_log.txt", "a", encoding="utf-8") as f:
+        f.write("用户：" + question + "\n")
+        f.write("模型：" + answer + "\n")
+        f.write("------\n")
+
 print("程序结束")
