11b91ae88d7bb0628381a1a7079f66e7c474d1de
Developer <developer@example.com>
2026-06-22 18:19:54 +0800

    V1.0 整理成可复用命令行工具

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from llm_client import ask_deepseek
 
 
-while True:
+def main():
+    while True:
 
-    question = input("请输入你的问题，输入q退出：")
+        question = input("请输入你的问题，输入q退出：")
 
-    if question == "q":
-        break
+        if question == "q":
+            break
 
-    answer = ask_deepseek(question)
-    print(answer)
+        answer = ask_deepseek(question)
+        print(answer)
 
-    with open("chat_log.txt", "a", encoding="utf-8") as f:
-        f.write("用户：" + question + "\n")
-        f.write("模型：" + answer + "\n")
-        f.write("------\n")
+        with open("chat_log.txt", "a", encoding="utf-8") as f:
+            f.write("用户：" + question + "\n")
+            f.write("模型：" + answer + "\n")
+            f.write("------\n")
 
-print("程序结束")
+    print("程序结束")
+
+
+if __name__ == "__main__":
+    main()
